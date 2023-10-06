eef50ccb2c0589a534e5f9696527d0e4941837b9
explicit <113419796+Untfoubles@users.noreply.github.com>
2023-10-06 16:49:34 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,9 @@
+import pyautogui
+import time 
+
+pyautogui.click(1800, 40)
+pyautogui.click(1400, 60, clicks=3)
+pyautogui.write('https://soundcloud.com/sheb6l/cash-shit?in=a-y-75033887/sets/in-luv/')
+pyautogui.hotkey('enter')
+# time.sleep(5)
+pyautogui.hotkey('spacebar')
